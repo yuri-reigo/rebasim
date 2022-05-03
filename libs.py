742fbba574bb40e4edc2955e9d280911946480e5
Yuri S <yuri@reigo-inv.com>
2022-05-03 11:31:55 +0300

    Sending data to API

--- libs.py
+++ libs.py
@@ -3,3 +3,6 @@ def my_print(str):
 
 def my_add(a, b):
 	return a + b
+	
+def send_to_api(data):
+	print(f"Sending '{data}' to API")
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from libs import my_print, my_add
+from libs import my_print, my_add, send_to_api
 
 def main():
     num = my_add(1, 2)
     my_print(f"The number is ....: {num} !")
-
+    send_to_api("work complete")
 
 if __name__ == "__main__":
     main()
