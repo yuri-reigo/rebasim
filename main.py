ed8ab5bc54a8674573180f6a9f51689a19d75386
yuri-reigo <101973683+yuri-reigo@users.noreply.github.com>
2022-05-03 16:49:00 +0300

    Added API comms (#4)

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from libs import my_print, my_add
+from libs import my_print, my_add, send_to_api
 
 def main():
     num = my_add(1, 2)
     my_print(f"My num is: {num}!")
+    send_to_api("Some data")
 
 
 if __name__ == "__main__":
