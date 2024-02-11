61eb864b68b08bbb12224e0a175ef25fa3f9760d
Yuri Shmorgun <yuri@reigo-inv.com>
2024-02-11 11:53:58 +0200

    Updated main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from libs import my_print, my_add, send_to_api
 
 def main():
+    print("entering main...")
     num = my_add(1, 2)
     my_print(f"My num is: {num}!")
     send_to_api("Some data")
